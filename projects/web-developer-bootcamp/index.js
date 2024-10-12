f9653df81c716f12e48fae11f00fe56b46b95afd
Iyare Usen <iyare.usen@gmail.com>
2024-10-12 10:00:20 +0100

    todo list updated

---
+++ projects/web-developer-bootcamp/index.js
@@ -0,0 +1,7 @@
+let students = ["Iyare", "Judah", "Nathan", "Olivia", "Joshua"];
+
+console.log(students.length);
+
+students[students.length ] = "Osaze";
+console.log(students);
+console.log(students.length);
