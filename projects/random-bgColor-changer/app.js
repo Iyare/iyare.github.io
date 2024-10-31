e626eceb928b1ff930b36ac924cdb567645e8e8b
Iyare Usen <iyare.usen@gmail.com>
2024-10-31 17:44:45 +0100

    Adds random color changer project

---
+++ projects/random-bgColor-changer/app.js
@@ -0,0 +1,22 @@
+const changeBtn = document.querySelector("#changeBg");
+
+const body = document.querySelector("body");
+
+changeBtn.addEventListener("click", () => {
+    // making a random red channel value
+    const red = Math.floor((Math.random() * 255) + 1);
+    
+        // making a random green channel value
+     const green = Math.floor((Math.random() * 255) + 1);
+    
+    
+        // making a random blue channel value
+     const blue = Math.floor((Math.random() * 255) + 1);
+    
+    // Combine the channels
+    const rgbValue = `rgb(${red},${green},${blue})` 
+    
+    // Update the background color
+    body.style.backgroundColor = rgbValue;
+    
+})
